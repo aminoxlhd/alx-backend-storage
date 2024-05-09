db00d76b1f0d998b2eecd58e9d73ef9d45847333
aminoxlhd <aminoxlhd@gmail.com>
2024-05-09 23:02:08 +0000

    edit 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,17 +1,10 @@
 -- creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
-drop procedure IF EXISTS ComputeAverageScoreForUser;
-DELIMITER $$ ;
-CREATE PROCEDURE ComputeAverageScoreForUser(
-	IN user_id INT
-)
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-	UPDATE users
-	SET average_score = (
-		SELECT AVG(score)
-		FROM corrections
-		WHERE user_id = user_id
-	)
-	WHERE users.id = user_id;
-
-END;$$
+    UPDATE users
+    SET average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+    WHERE id = user_id;
+END;//
 DELIMITER ;
