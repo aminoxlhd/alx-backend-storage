c20cd213c6daa66a3f75a64a472340a997ba3a95
aminoxlhd <aminoxlhd@gmail.com>
2024-05-14 19:37:49 -0700

    add 102-log_stats.py

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" Log stats - new version """
+
+from pymongo import MongoClient
+
+
+def log_stats(mongo_collection):
+    """ log_stats function """
+    print(f"{mongo_collection.count_documents({})} logs")
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = mongo_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+    print(f"{mongo_collection.count_documents({'method': 'GET', 'path': '/status'})} status check")
+    print("IPs:")
+    pipeline = [
+            {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+            {"$sort": {"count": -1}},
+            {"$limit": 10}
+    ]
+    ips = mongo_collection.aggregate(pipeline)
+    for ip in ips:
+        print(f"\t{ip['_id']}: {ip['count']}")
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+    log_stats(collection)
