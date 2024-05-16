874558f715763167b603ba2f6a4703814e123a8d
aminoxlhd <aminoxlhd@gmail.com>
2024-05-16 00:09:07 +0000

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import uuid
 import redis
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -18,3 +18,21 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> UnionOfTypes:
+        """ get function """
+        val = self._redis.get(key)
+        if fn:
+            val = fn(val)
+        return val
+
+
+    def get_str(self, key: str) -> str:
+        """ get_str function """
+        return self.get(key, fn=str)
+
+
+    def get_int(self, key: str) -> int:
+        """ get_int function """
+        return self.get(key, fn=int)
