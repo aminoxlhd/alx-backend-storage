19ba71cd8f87642498bec7afc7e960748d100025
aminoxlhd <aminoxlhd@gmail.com>
2024-05-16 01:34:13 +0000

    edit task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,7 +13,7 @@ def count_calls(method: Callable) -> Callable:
     @functools.wraps(method)
     def wrapper(self, *args, **kwargs):
         """ wrapper function """
-        self.redis.incr(method.__qualname__)
+        self._redis.incr(method.__qualname__)
         return method(self, *args, **kwargs)
 
     return wrapper
